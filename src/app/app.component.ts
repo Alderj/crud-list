1c668587fca119868979b6c19cf7156687faffdb
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-23 14:57:47 -0400

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,22 @@ import { Employee } from './models/Employee';
 })
 export class AppComponent {
   employeeArray: Employee[] = [
-    {id:1, name:"Alder", country:"Brazil"},
-    {id:2, name:"Madonna", country:"USA"},
-    {id:3, name:"Puttin", country:"Russia"}
+    { id: 1, name: "Alder", country: "Brazil" },
+    { id: 2, name: "Madonna", country: "USA" },
+    { id: 3, name: "Puttin", country: "Russia" }
   ]
 
   selectedEmployee: Employee = new Employee();
 
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
 }
